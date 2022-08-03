b06d4c8b5299c40c0b4956ad6a95c228e4045166
Hiepowork <101622596+Hiepowork@users.noreply.github.com>
2022-08-03 14:01:42 -0500

    Add files via upload

---
+++ WeatherApp/script.js
@@ -0,0 +1,47 @@
+let weather = {
+    apiKey : "a8aab94f5079396bfe1677efd3df4520",
+    fetchWeather: function(city){
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=metric&appid=" 
+            + this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data){
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        console.log(name,icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".temp").innerText = temp + " °C";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity+"%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".weather").classList.remove("loading");
+    },
+
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value)
+    }
+};
+
+
+document
+    .querySelector(".search button")
+    .addEventListener("click", function(){
+        weather.search();
+    });
+
+document.querySelector(".search-bar")
+    .addEventListener("keyup", function(event){
+        if (event.key == "Enter"){
+            weather.search();
+        }
+    })
+
+weather.fetchWeather("Washington D.C.");
